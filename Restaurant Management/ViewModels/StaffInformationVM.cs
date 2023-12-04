d5ad0e0a1e9baad6c429ba12b1a857266681ba50
Thanh Tai Nguyen <tai606760@gmail.com>
2023-12-05 04:37:16 +0700

    Update StaffInformationVM.cs

--- Restaurant Management/ViewModels/StaffInformationVM.cs
+++ Restaurant Management/ViewModels/StaffInformationVM.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Restaurant_Management.ViewModels
 {
-    internal class StaffInformationVM
+    public class StaffInformationVM : Utilities.ViewModelBase
     {
     }
 }
