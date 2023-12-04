138ddfc4d0a86642ca06d57e9664f64f0144915a
Thanh Tai Nguyen <tai606760@gmail.com>
2023-12-04 20:11:52 +0700

    Utilities folder update

---
+++ Restaurant Management/Utilities/Btn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+namespace Restaurant_Management.Utilities
+{
+    public class Btn : RadioButton
+    {
+        static Btn()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Btn), new System.Windows.FrameworkPropertyMetadata(typeof(Btn)));  
+        }
+    }
+}
---
+++ Restaurant Management/Utilities/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace Restaurant_Management.Utilities
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
