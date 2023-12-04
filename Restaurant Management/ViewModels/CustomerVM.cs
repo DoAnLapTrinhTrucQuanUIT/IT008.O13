6abd771cbf817c3e00937ddcc850d2740a095e2a
Thanh Tai Nguyen <tai606760@gmail.com>
2023-12-05 03:41:52 +0700

    Update CustomerVM.cs

--- Restaurant Management/ViewModels/CustomerVM.cs
+++ Restaurant Management/ViewModels/CustomerVM.cs
@@ -8,16 +8,6 @@ namespace Restaurant_Management.ViewModels
 {
     class CustomerVM : Utilities.ViewModelBase
     {
-        private readonly PageModel _pageModel;
-        public int CustomerID
-        {
-            get { return _pageModel.CustomerCount; }
-            set { _pageModel.CustomerCount = value; OnPropertyChanged(); }
-        }
-        public CustomerVM()
-        {
-            _pageModel = new PageModel();
-            CustomerID = 22521337;
-        }
+
     }
 }
