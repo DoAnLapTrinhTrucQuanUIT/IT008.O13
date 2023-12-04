acdddb54bd5801ed41f88b2c55b220f531740321
Thanh Tai Nguyen <tai606760@gmail.com>
2023-12-05 04:37:18 +0700

    Update TableVM.cs

--- Restaurant Management/ViewModels/TableVM.cs
+++ Restaurant Management/ViewModels/TableVM.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Restaurant_Management.ViewModels
 {
-    internal class TableVM
+    public class TableVM : Utilities.ViewModelBase
     {
+        
     }
 }
