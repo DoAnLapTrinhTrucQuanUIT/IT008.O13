e2b5115f2eb0b5975c7f6b13fdd93ad6d123d479
Thanh Tai Nguyen <tai606760@gmail.com>
2023-12-04 20:12:59 +0700

    Update ViewModelBase.cs

--- Restaurant Management/ViewModels/ViewModelBase.cs
+++ Restaurant Management/ViewModels/ViewModelBase.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Restaurant_Management.ViewModels
 {
@@ -11,9 +13,45 @@ namespace Restaurant_Management.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChange(string propertyName)
+        protected virtual void OnPropertyChange([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        class RelayCommand<T> : ICommand
+        {
+            private readonly Predicate<T> _canExecute;
+            private readonly Action<T> _execute;
+
+            public RelayCommand(Predicate<T> canExecute, Action<T> execute)
+            {
+                if (execute == null)
+                    throw new ArgumentNullException("execute");
+                _canExecute = canExecute;
+                _execute = execute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                try
+                {
+                    return _canExecute == null ? true : _canExecute((T)parameter);
+                }
+                catch
+                {
+                    return true;
+                }
+            }
+
+            public void Execute(object parameter)
+            {
+                _execute((T)parameter);
+            }
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
         }
     }
 }
