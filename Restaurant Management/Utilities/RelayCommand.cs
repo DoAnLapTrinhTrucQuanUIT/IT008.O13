a648b8e84b9a5b7a1223d6d9474d8816a07359ac
Thanh Tai Nguyen <tai606760@gmail.com>
2023-12-05 02:26:07 +0700

    Update RelayCommand.cs

--- Restaurant Management/Utilities/RelayCommand.cs
+++ Restaurant Management/Utilities/RelayCommand.cs
@@ -17,7 +17,7 @@ namespace Restaurant_Management.Utilities
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
             _execute = execute;
             _canExecute = canExecute;
