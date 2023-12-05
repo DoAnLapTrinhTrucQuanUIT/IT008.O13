7a41b3b341a7fbb9f23aab273b6d291f8bfd2d0f
Thanh Tai Nguyen <tai606760@gmail.com>
2023-12-05 20:29:52 +0700

    Update CustomerVM.cs

--- Restaurant Management/ViewModels/CustomerVM.cs
+++ Restaurant Management/ViewModels/CustomerVM.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Restaurant_Management.Models;
 namespace Restaurant_Management.ViewModels
 {
     public class CustomerVM : Utilities.ViewModelBase
