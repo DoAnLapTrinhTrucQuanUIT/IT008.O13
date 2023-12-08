731d90c1335fcd888465533cae58868d3f7e581c
qthawq <nguyenqthangwork@gmail.com>
2023-12-09 01:22:11 +0700

    ThangPush

--- Restaurant Management/Views/Component/Customer.xaml.cs
+++ Restaurant Management/Views/Component/Customer.xaml.cs
@@ -10,18 +10,57 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Restaurant_Management.Views.Component
 {
-    /// <summary>
-    /// Interaction logic for Customer.xaml
-    /// </summary>
-    public partial class Customer : Window
+    public partial class Customer : UserControl
     {
         public Customer()
         {
             InitializeComponent();
+            DataContext = this;
         }
+
+        public static readonly DependencyProperty TextHeaderProperty = DependencyProperty.Register("TextHeader", typeof(string), typeof(Customer), new PropertyMetadata(""));
+        public string TextHeader
+        {
+            get { return (string)GetValue(TextHeaderProperty); }
+            set { SetValue(TextHeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextofNumberProperty = DependencyProperty.Register("TextofNumber", typeof(string), typeof(Customer), new PropertyMetadata(""));
+        public string TextofNumber
+        {
+            get { return (string)GetValue(TextofNumberProperty); }
+            set { SetValue(TextofNumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextofPercentProperty = DependencyProperty.Register("TextofPercent", typeof(string), typeof(Customer), new PropertyMetadata(""));
+        public string TextofPercent
+        {
+            get { return (string)GetValue(TextofPercentProperty); }
+            set { SetValue(TextofPercentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImagePathStatusProperty =
+            DependencyProperty.Register("ImagePathStatus", typeof(string), typeof(Customer));
+
+        public string ImagePathStatus
+        {
+            get { return (string)GetValue(ImagePathStatusProperty); }
+            set { SetValue(ImagePathStatusProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImagePathSymbolProperty =
+            DependencyProperty.Register("ImagePathSymbol", typeof(string), typeof(Customer));
+
+        public string ImagePathSymbol
+        {
+            get { return (string)GetValue(ImagePathSymbolProperty); }
+            set { SetValue(ImagePathSymbolProperty, value); }
+        }
+
     }
 }
