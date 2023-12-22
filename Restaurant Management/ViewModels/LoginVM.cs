9bd144a72fe3d1b003c352347a87ea76877d4ff8
qthawq <nguyenqthangwork@gmail.com>
2023-12-22 21:00:46 +0700

    ThangPush

---
+++ Restaurant Management/ViewModels/LoginVM.cs
@@ -0,0 +1,127 @@
+﻿using Restaurant_Management.Utilities;
+using Restaurant_Management.Views.Component;
+using Restaurant_Management.Views;
+using Restaurant_Management.Utilities.MongoDBManager;
+using System;
+using System.Text;
+using System.Windows.Input;
+using System.Windows;
+using System.Security.Cryptography;
+using System.Windows.Controls;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Restaurant_Management.Models;
+using System.Collections.ObjectModel;
+
+namespace Restaurant_Management.ViewModels
+{
+    public class LoginVM : Utilities.ViewModelBase
+    {
+        private readonly LoginWindow _loginWindow;
+        public LoginVM(LoginWindow loginWindow)
+        {
+            _loginWindow = loginWindow ?? throw new ArgumentNullException(nameof(loginWindow));
+        }
+        public static bool IsLogin { get; set; }
+        private string _username;
+        private string _password;
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public ICommand LoginCM { get; set; }
+        public ICommand PasswordchangeCM { get; set; }
+        public ICommand ForgetpasswordCM { get; set; }
+        
+        public LoginVM()
+        {
+            IsLogin = false;
+            Password = "";
+            Username = "";
+            LoginCM = new RelayCommand<LoginWindow>((p) => true, (p) => _Login(p));
+            PasswordchangeCM = new RelayCommand<PasswordBox>((p) => true, (p) => { Password = p.Password; });
+            ForgetpasswordCM = new RelayCommand<LoginWindow>((p) => true, (p) => _ForgetPassword(p));
+        }
+
+        void _Login(LoginWindow p)
+        {
+            var hashedPassword = HashPassword(_password);
+            try
+            {
+                if (p == null) return;
+
+                var dbContext = new MongoDbContext("mongodb+srv://taint04:H20YQ9j6nvKXiaoA@tai-server.0x4tojd.mongodb.net/", "Restaurant_Management_Application");
+
+                var username_filter = Builders<Account>.Filter.Eq("Username", Username);
+                var username = dbContext.Accounts.Find(username_filter).FirstOrDefault();
+                if (username == null)
+                {
+                    MessageBox.Show("Username is not exist!", "Announcement", MessageBoxButton.OK);
+                }
+                else
+                {
+                    var acc_filter = Builders<Account>.Filter.Eq("Username", Username) & Builders<Account>.Filter.Eq("Password", HashPassword(Password));
+                    var account = dbContext.Accounts.Find(acc_filter).FirstOrDefault();
+                    if (account != null)
+                    {
+                        IsLogin = true;
+                        Const.UserName = Username;
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        Username = "";
+                        Password = "";
+                        p.Visibility = Visibility.Collapsed;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password is incorrect!", "Announcement", MessageBoxButton.OK);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Announcement", MessageBoxButton.OK);
+            }
+        }
+
+
+        void _ForgetPassword(LoginWindow parameter)
+        {
+            ForgetPasswordView forgetPassControl = new ForgetPasswordView();
+
+            Window forgetPassWindow = new Window
+            {
+                Content = forgetPassControl,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                WindowStyle = WindowStyle.None,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen
+            };
+
+            forgetPassWindow.ShowDialog();
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
---
+++ Restaurant Management/Views/LoginWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using MongoDB.Driver;
+using Restaurant_Management.Models;
+using Restaurant_Management.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant_Management.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private readonly IMongoCollection<Account> _Account;
+        public LoginWindow()
+        {
+            InitializeComponent();
+            _Account = GetMongoCollection();
+        }
+        private IMongoCollection<Account> GetMongoCollection()
+        {
+            // Set your MongoDB connection string and database name
+            string connectionString =
+                "mongodb+srv://taint04:H20YQ9j6nvKXiaoA@tai-server.0x4tojd.mongodb.net/"; // Update with your MongoDB server details
+            string databaseName = "Restaurant_Management_Application"; // Update with your database name
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+
+            return database.GetCollection<Account>("Account");
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedUsername = username.Text;
+            var enteredPassword = password.Password;
+
+            if (selectedUsername != null && !string.IsNullOrEmpty(enteredPassword))
+            {
+                var user = GetUser(selectedUsername);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Sai tên tài khoản hoặc mật khẩu.");
+                    return;
+                }
+
+                if (enteredPassword == user.Password)
+                {
+                    // Password is valid, grant access
+
+                    // You can open the main window or perform other actions here.
+                    // Open the main window
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+
+                    // Close the login window
+                    this.Close();
+                }
+                else
+                {
+                    // Password is invalid
+                    MessageBox.Show("Sai tên tài khoản hoặc mật khẩu.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin.");
+            }
+        }
+
+        private Account GetUser(string username)
+        {
+            var filter = Builders<Account>.Filter.Eq(u => u.Username, username);
+            return _Account.Find(filter).FirstOrDefault();
+        }
+    }
+}
