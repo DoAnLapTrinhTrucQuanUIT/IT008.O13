f5c78cde83a1ad0384ebe0f5af744f75125e0041
qthawq <nguyenqthangwork@gmail.com>
2023-12-25 00:40:49 +0700

    ThangPush

--- Restaurant Management/ViewModels/StaffInformationVM.cs
+++ Restaurant Management/ViewModels/StaffInformationVM.cs
@@ -1,12 +1,96 @@
 ﻿using System;
+using MongoDB.Driver;
+using Restaurant_Management.Models;
+using Restaurant_Management.Utilities;
+using Restaurant_Management.Views;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using Restaurant_Management.Views.Component;
+using System.Xml.Linq;
 
 namespace Restaurant_Management.ViewModels
 {
     public class StaffInformationVM : Utilities.ViewModelBase
     {
+        private string _fullname;
+        public string FullName
+        {
+            get => _fullname;
+            set
+            {
+                _fullname = value;
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        private string _isAdmin;
+        public string IsAdmin
+        {
+            get => _isAdmin;
+            set
+            {
+                _isAdmin = value;
+                OnPropertyChanged(nameof(IsAdmin));
+            }
+        }
+
+        private BitmapImage _avatarimagesource;
+        public BitmapImage AvatarImageSource
+        {
+            get => _avatarimagesource;
+            set
+            {
+                _avatarimagesource = value;
+                OnPropertyChanged(nameof(AvatarImageSource));
+            }
+        }
+        public ICommand LoadWindowCommand { get; set; }
+
+        private readonly IMongoCollection<Employees> _Employees;
+
+        public StaffInformationVM()
+        {
+            _Employees = GetMongoCollection();
+            LoadWindowCommand = new RelayCommand<NavigationBar>((p) => true, (p) => _LoadWindow());
+        }
+        private IMongoCollection<Employees> GetMongoCollection()
+        {
+            // Set your MongoDB connection string and database name
+            string connectionString =
+                "mongodb+srv://taint04:H20YQ9j6nvKXiaoA@tai-server.0x4tojd.mongodb.net/"; // Update with your MongoDB server details
+            string databaseName = "Restaurant_Management_Application"; // Update with your database name
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+
+            return database.GetCollection<Employees>("Employees");
+        }
+        void _LoadWindow()
+        {
+            string employeeId = Const.UserID;
+
+            var filter = Builders<Employees>.Filter.Eq(x => x.EmployeeId, employeeId);
+            var User = _Employees.Find(filter).FirstOrDefault();
+
+            if (User != null)
+            {
+                FullName = User.FullName;
+                switch (User.IsAdmin)
+                {
+                    case true:
+                        IsAdmin = "Admin";
+                        break;
+                    case false:
+                        IsAdmin = "Employee";
+                        break;
+                }
+                AvatarImageSource = User.AvatarImageSource;
+            }
+        }
     }
 }
--- Restaurant Management/Views/Component/NavigationBar.xaml.cs
+++ Restaurant Management/Views/Component/NavigationBar.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using MongoDB.Driver;
+using Restaurant_Management.Models;
+using Restaurant_Management.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +17,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace Restaurant_Management.Views.Component
 {
-    /// <summary>
-    /// Interaction logic for NavigationBar.xaml
-    /// </summary>
     public partial class NavigationBar : UserControl
     {
         public NavigationBar()
         {
             InitializeComponent();
+        }
+       
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Minimize the main window
+            if (Window.GetWindow(this) is Window mainWindow)
+            {
+                mainWindow.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the main window
+            if (Window.GetWindow(this) is Window mainWindow)
+            {
+                mainWindow.Close();
+            }
         }
     }
 }
