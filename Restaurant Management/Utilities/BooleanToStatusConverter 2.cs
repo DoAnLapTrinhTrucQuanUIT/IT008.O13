839e36f4ba6a1b77b3c53b3b8be002d3718ebe65
qthawq <nguyenqthangwork@gmail.com>
2023-12-30 00:22:39 +0700

    ThangCommit

---
+++ Restaurant Management/Utilities/BooleanToStatusConverter 2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Restaurant_Management.Utilities
+{
+    public class BooleanToStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool status && status)
+            {
+                return "Paid";
+            }
+            else
+            {
+                return "Unpaid";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
