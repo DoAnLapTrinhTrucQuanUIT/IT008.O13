cfc49e416d578376b1895b13ede53bedfe6756ea
qthawq <nguyenqthangwork@gmail.com>
2024-01-01 16:33:13 +0700

    ThangPush

--- Restaurant Management/ViewModels/UpdateProfileVM.cs
+++ Restaurant Management/ViewModels/UpdateProfileVM.cs
@@ -98,6 +98,7 @@ namespace Restaurant_Management.ViewModels
                 if (result.IsModifiedCountAvailable && result.ModifiedCount > 0)
                 {
                     MessageBox.Show("Profile updated successfully!", "Notification");
+
                     var mainWindow = App.Current.MainWindow;
                     App.Current.MainWindow = null;
                     mainWindow.Close();
